148a8d367e35b043adedc80545a85a21dccb845e
Farid <farid.rahimoff@gmail.com>
2021-01-14 09:24:59 +0400

    dino json loading and Webpack configuration

---
+++ script/app.js
@@ -0,0 +1,42 @@
+
+    // Create Dino Constructor
+function Dino (){
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+}
+
+    // Create Dino Objects
+Dino1= Object.create(Dino);
+let fs = require ('./dino.json')
+console.log (fs);
+
+    // Create Human Object
+
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+    // NOTE: Weight in JSON file is in lbs, height in inches. 
+
+    
+    // Create Dino Compare Method 2
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+    
+    // Create Dino Compare Method 3
+    // NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+    // Generate Tiles for each Dino in Array
+  
+        // Add tiles to DOM
+
+    // Remove form from screen
+
+
+// On button click, prepare and display infographic
